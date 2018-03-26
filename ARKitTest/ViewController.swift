91b11acd2c8b6cc96c0f52cb7a06b3a46130b8cc
Kacper Martela <kacper99@hotmail.co.uk>
2018-03-26 22:54:43 +0100

    Connected scene view to controller

--- ARKitTest/ViewController.swift
+++ ARKitTest/ViewController.swift
@@ -7,17 +7,15 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
