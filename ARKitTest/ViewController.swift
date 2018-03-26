88c4ac415baeaec77f067b2ddaea9354e8b0fc9c
Kacper Martela <kacper99@hotmail.co.uk>
2018-03-26 23:18:42 +0100

    Added a function to add a box

--- ARKitTest/ViewController.swift
+++ ARKitTest/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     /**
@@ -27,9 +26,27 @@ class ViewController: UIViewController {
         sceneView.session.run(configuration) //Session manages motion tracking and image processing from camera. Running it with our configuration
     }
     
+    /**
+    Tells the AR session to stop/pause
+     */
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillAppear(animated)
         sceneView.session.pause() //Tell the AR session to stop tracking motion and processing image
+    }
+    
+    /**
+    Used to add a box into the enviroment
+    */
+    func addBox(){
+        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0) //The box to add, can make edges rounded as well
+        
+        let boxNode = SCNNode() //Represents the position and coordinates of an object in 3D space
+        boxNode.geometry = box //Set the nodes content to be the box
+        boxNode.position = SCNVector3(0,0,-0.2) //Position is relative to the camera, therefore we place it 0.2m infront of us.
+        
+        let scene = SCNScene()
+        scene.rootNode.addChildNode(boxNode)
+        sceneView.scene = scene
     }
 }
 
